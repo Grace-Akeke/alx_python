568c31ac429f7639258ac31f7ec009eb021e2d69
Grace-Akeke <graceakeke@gmail.com>
2023-07-24 01:29:01 +0100

    task 1

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -4,9 +4,4 @@ word = "Holberton"
 print("First 3 letters: Hol")
 print("Last 2 letters: on")
 print("Middle word: olberto")
-#!/usr/bin/python3
-word = "Holberton"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print("First 3 letters: sch")
-print("Last 2 letters: ol")
-print("Middle word: choo")
+
