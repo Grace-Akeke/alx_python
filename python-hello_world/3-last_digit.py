8221f0fa7726816b58b6945366377914960a4325
Grace-Akeke <graceakeke@gmail.com>
2023-07-28 22:15:46 +0100

    last digit modified

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -9,6 +9,6 @@ if last_digit > 5:
     print("Last digit of", number, "is", last_digit, "and is greater than 5")
 if last_digit == 0:
      print("Last digit of", number, "is", last_digit, "and is 0")
-if last_digit < 6 != 0:
+else:
       print("Last digit of", number, "is", last_digit, "and is less than 6 and not 0")
  
