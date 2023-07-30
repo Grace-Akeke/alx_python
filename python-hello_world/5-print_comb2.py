fcd5dfd6623141b9e20615846eaa0f256a428ea9
Grace-Akeke <graceakeke@gmail.com>
2023-07-30 23:58:40 +0100

    task 5

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-    if i == 99:
-        print (f"{i:02d}\n")
+    if i <= 9:
+        print("0{}".format(i), end=", ")
     else:
-        print(f"{i:02d}, ", end="")
+        print("{}, ".format(i) if i < 99  else "{}\n".format(i), end="")
