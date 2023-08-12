44626a52cd191ac5b9659416db29bdf9eccad688
Grace-Akeke <graceakeke@gmail.com>
2023-08-12 18:02:04 +0100

    fibonacci numbers

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,18 +1,16 @@
-#!/usr/bin/env python3
 def fibonacci_sequence(n):
-   if n <= 0:
-      return []
-   elif n == 1:
-      return [0]
-   fibonacci_numbers=[0,1]
-   while len(fibonacci_numbers) < n:
-      next_number = fibonacci_numbers[-1] + fibonacci_numbers[-2]
-      if next_number <= 0:
-         return []
-   fibonacci_numbers.append(next_number)
-   return fibonacci_numbers
-if __name__ =="__main__":
-   fibonacci_sequence = __import__('4-fibonacci').fibonacci_sequences
+    if n <= 0:
+        return []
+
+    fibonacci_numbers = []
+    a, b = 0, 1
+
+    while n > 0:
+        fibonacci_numbers.append(a)
+        a, b = b, a + b
+        n -= 1
+
+    return fibonacci_numbers
       
 
    
