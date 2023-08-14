3c17cbaade780a181db331997c266d7f668e3582
Grace-Akeke <graceakeke@gmail.com>
2023-08-14 22:38:15 +0100

    importation of a add function

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,11 @@
-# Assign values to variables 'a' and 'b' in separate lines
-a = 1
-b = 2
-result = add(a, b)
-if __name__ == "__main__":
-# Import the 'add' function from add_0.py
-    from add_0 import add
+#!/usr/bin/python3
+from add_0 import add
 
-# Print the result of the addition using string format
-print("{} + {} = {}".format(a, b, result))
+def main():
+    a = 1
+    b = 2
+    result = add(a, b)
+    print('{} + {} = {}'.format (a, b, result))
+    #check if the script is running directly
+    if __name__ == "__main__":
+        main()
