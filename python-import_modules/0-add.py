58fbf4ed99d23665e78c9b36ff5ed82599015077
Grace-Akeke <graceakeke@gmail.com>
2023-08-14 22:34:16 +0100

    task 1

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,9 @@
 # Assign values to variables 'a' and 'b' in separate lines
 a = 1
 b = 2
-
+if __name__ == "__main__":
 # Import the 'add' function from add_0.py
-from add_0 import add
+    from add_0 import add
 
 # Print the result of the addition using string format
 print("{} + {} = {}".format(a, b, add(a, b)))
