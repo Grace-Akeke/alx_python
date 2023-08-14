2b4517d899b990888411f9fe057a859199e490cd
Grace-Akeke <graceakeke@gmail.com>
2023-08-14 21:41:41 +0100

    0_add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -3,7 +3,7 @@ a = 1
 b = 2
 
 # Import the 'add' function from add_0.py
-add = __import__('add_0').add
+from add_0 import add
 
 # Print the result of the addition using string format
 print("{} + {} = {}".format(a, b, add(a, b)))
