1512455cce85b87f9fc077324320ef6bd1d8eeda
Grace-Akeke <graceakeke@gmail.com>
2023-08-14 22:40:51 +0100

    importing addition

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,10 @@
 # Assign values to variables 'a' and 'b' in separate lines
 a = 1
 b = 2
+result = add(a, b)
 if __name__ == "__main__":
 # Import the 'add' function from add_0.py
     from add_0 import add
 
 # Print the result of the addition using string format
-print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} + {} = {}".format(a, b, result))
