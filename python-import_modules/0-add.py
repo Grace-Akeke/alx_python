0360da49f2b6fd8495993bccf44e15d7c66ef0d5
Grace-Akeke <graceakeke@gmail.com>
2023-08-14 22:10:06 +0100

    importation of the addition function

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,11 +1,12 @@
-#!/usr/bin/python3
 from add_0 import add
+
 
 def main():
     a = 1
     b = 2
     result = add(a, b)
-    print("{} + {} = {}".format (a, b, result))
-    #check if the script is running directly
-    if __name__ == "__main__":
-        main()
+    print("{} + {} = {}".format(a, b,result))
+   
+#check if the main script is one running
+if __name__ == "__main__":
+    main()
