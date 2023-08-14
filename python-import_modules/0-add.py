f4ad8b548911b4dc9b267f33c0a1ce4dd727eae8
Grace-Akeke <graceakeke@gmail.com>
2023-08-14 22:47:50 +0100

    importing 0-add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -5,7 +5,7 @@ def main():
     a = 1
     b = 2
     result = add(a, b)
-    print('{} + {} = {}'.format (a, b, result))
+    print("{} + {} = {}".format (a, b, result))
     #check if the script is running directly
     if __name__ == "__main__":
         main()
