9d5c3417f33cd4cc0f6c8d9bbf1754c54fce64b1
Grace-Akeke <graceakeke@gmail.com>
2023-08-15 22:26:23 +0100

    listing the values of an argument

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -6,9 +6,9 @@ def main():
     num_args = len(sys.argv) - 1
     #print number of arguements and their values
     if num_args == 0:
-        print("0 arguments")
+        print("0 arguments.")
     elif num_args == 1:
-        print("1 argument")
+        print("1 argument:")
         print("1: {}".format(sys.argv[1]))
     else:
         print("{} arguments:".format(num_args))
