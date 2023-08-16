028d70db948ad32ec5a273bf0caccbf5def44f58
Grace-Akeke <graceakeke@gmail.com>
2023-08-16 21:57:29 +0100

    raise exception

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -2,6 +2,3 @@ def raise_exception():
     value = "Hello, World!"
     if not isinstance(value, int):
         raise TypeError("Expection has been raised")
-
-# Calling the function to raise the exception
-raise_exception()
